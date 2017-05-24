b5d9a22c9d1afb4e61ca03b427f962c56a589500
m-warman <matthewwarman@gmail.com>
2017-05-24 13:29:17 -0700

    Added bed6 format output, window sizes

--- stop_codon_finder.py
+++ stop_codon_finder.py
@@ -4,13 +4,15 @@
 # May 12, 2017
 
 # This script finds stop codon positions in ordered gff3 files and associates 
-# one stop codon position with one gene. Output is a four column 
-# tab-delimited file (output.txt):
+# one stop codon position with one gene. It then makes a window around the stop
+# codon and outputs one window for each stop codon in a tab-delimited BED6 file:
 #
-# <1>   Chromosome number
-# <2>   Gene ID
-# <3>   Stop codon site
-# <4>   Strand
+# <1>   Chrom
+# <2>   Start 
+# <3>   End
+# <4>   Name (Gene name)
+# <5>   Score (1 is used as a placeholder)
+# <6>   Strand
 #
 # Usage:
 # stop_codon_finder.py [genome_annotations.gff3]
@@ -24,11 +26,16 @@ if len(sys.argv) == 1:
     print("\nNo files supplied.\n\nUsage: stop_codon_finder.py [FILE]\n")
     quit()
 
+# Sets the window size. Here upstream referes to towards to coding region of
+# the gene, while downstream refers to the 3' UTR region:
+window_upstream_size = 200
+window_downstream_size = 100
+
 # Creates some lists to store important info. The script processes each line
 # individually, but doesn't know which CDS will be the end CDS until it
 # processes all the CDSs for that gene, so it has to store the information for
 # all the CDSs in each gene until it reads through that entire gene, then it
-# can pull out the gene name from the list and the stop site, then delete the
+# can pull out the gene name from the list and the stop site, and delete the
 # contents of the lists.
 current_gene_name_list = list()
 current_CDS_start_list = list()
@@ -39,7 +46,7 @@ list_pos_counter = 0
 
 # Opens the gff3 file and first separates into a list based on \t
 fandle = io.open(sys.argv[1], "rU")
-out_fandle = io.open("output.txt", "wb")
+out_fandle = io.open("test_output.bed", "wb")
 
 for line in fandle:
     linestripped = line.strip()
@@ -64,25 +71,38 @@ for line in fandle:
                     if re.search(r"_P01$", \
                      current_gene_name_list[list_pos_counter - 1]):
                         if current_strand_list[list_pos_counter - 1] == "+":
+                            current_window_start = (int(current_CDS_end_list[ \
+                             list_pos_counter - 1]) - \
+                             int(window_upstream_size)) 
+                            current_window_end = (int(current_CDS_end_list[ \
+                             list_pos_counter - 1]) + \
+                             int(window_downstream_size)) 
                             out_fandle.write(current_chr_list[ \
                              list_pos_counter - 1] + "\t")
+                            out_fandle.write(str(current_window_start) + "\t")
+                            out_fandle.write(str(current_window_end) + "\t")
                             out_fandle.write(current_gene_name_list[ \
                              list_pos_counter - 1] + "\t")
-                            out_fandle.write(current_CDS_end_list[ \
-                             list_pos_counter - 1] + "\t")
+                            out_fandle.write("1" + "\t")
                             out_fandle.write("+" + "\n")
                         if current_strand_list[list_pos_counter - 1] == "-":
+                            current_window_start = (int(current_CDS_start_list[ \
+                             0]) - int(window_downstream_size)) 
+                            current_window_end = (int(current_CDS_start_list[ \
+                             0]) + int(window_upstream_size)) 
                             out_fandle.write(current_chr_list[ \
                              list_pos_counter - 1] + "\t")
+                            out_fandle.write(str(current_window_start) + "\t")
+                            out_fandle.write(str(current_window_end) + "\t")
                             out_fandle.write(current_gene_name_list[ \
                              list_pos_counter - 1] + "\t")
-                            out_fandle.write(current_CDS_start_list[0] + "\t")
+                            out_fandle.write("1" + "\t")
                             out_fandle.write("-" + "\n")
                     # This stores the "current" gene, since the list is looking
-                    # back one gene. If it's not store then added to the black
+                    # back one gene. If it's not stored then added to the blank
                     # list, then it will delete everything and skip genes that
                     # have only 1 CDS. Does the same thing for CDS start and
-                    # end
+                    # end and strand.
                     gene_name_to_add = \
                      current_gene_name_list[len(current_gene_name_list) - 1]
                     CDS_start_to_add = \
